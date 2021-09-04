9ca8b5213698c62f3f899230b018503f0933cd6e
rewe999 <54740556+rewe999@users.noreply.github.com>
2021-09-04 13:43:07 +0200

    add tab/bar & FlatList

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {
   useFonts as useOswald,
@@ -10,6 +14,11 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrustructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurants.screen";
+
+const Tab = createBottomTabNavigator();
+
+const Map = () => <Text>Settings</Text>
+const Settings = () => <Text>Settings</Text>
 
 export default function App() {
 
@@ -25,10 +34,35 @@ export default function App() {
     return null;
   }
 
+  const screenOptions = ({route}) => ({})
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Restaurants') {
+                iconName = "md-restaurant"
+              } else if (route.name === 'Settings') {
+                iconName = "md-settings"
+              } else if (route.name === 'Map') {
+                iconName = "md-map"
+              }
+  
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurant/screens/restaurants.screen.js
+++ src/features/restaurant/screens/restaurants.screen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar, SafeAreaView, FlatList} from "react-native";
 import styled from "styled-components";
 
 import { RestaurantInfoCard } from "../../../components/restaurant-info-card.component";
@@ -14,19 +14,30 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  }
+})``;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar placeholder="Search" />
     </SearchContainer>
-
-    <RestaurantListContainer>
-        <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <RestaurantList 
+    data={[
+      {name: "1"},
+      {name: "2"},
+      {name: "3"},
+      {name: "4"},
+      {name: "5"},
+      {name: "6"},
+    ]}
+    renderItem={() => <RestaurantInfoCard />}
+    keyExtractor={(item) => item.name}
+    contentContainerStyle={{padding: 16}}
+    />
+        
   </SafeArea>
 );
